b0f2410398133d4d3ef85647b8e8fe6c8af84fe2
Keshav Tanwar <keshavtanwar1248@gmail.com>
2025-02-07 17:16:25 +0530

    Home Header and Progress Circle

---
+++ FitnessApp/HistoricDataView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricDataView.swift
+//  FitnessApp
+//
+//  Created by Sunil Tanwar on 07/02/25.
+//
+
+import SwiftUI
+
+struct HistoricDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricDataView()
+}
